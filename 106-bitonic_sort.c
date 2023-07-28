ce99d037f45e3dbcff36f801a9f07dc4760a4005
Prince <Princekarpture@gmail.com>
2023-07-28 11:28:46 -0700

    Bitonic sort

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,18 +1,19 @@
 /*
  * File: 106-bitonic_sort.c
- * Auth: Henrietta Obioha
+ * Auth: Brennan D Baraban
  */
 
 #include "sort.h"
 
 void swap_ints(int *a, int *b);
-void bitonic_merge(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow);
 void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
 void bitonic_sort(int *array, size_t size);
 
 /**
- * swap_ints - swap two integers in an array
- * @a: The first integer to swap
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
 void swap_ints(int *a, int *b)
@@ -25,14 +26,15 @@ void swap_ints(int *a, int *b)
 }
 
 /**
- * bitonic_merge - sort a bitonic sequence inside an array of integers
- * @array: An array of integers
- * @size: The size of the array
- * @start: The starting index of the sequence in array to sort
- * @seq: The size of the sequence to sort
- * @flow: The direction to sort in
+ * bitonic_merge - Sort a bitonic sequence inside an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ * @start: The starting index of the sequence in array to sort.
+ * @seq: The size of the sequence to sort.
+ * @flow: The direction to sort in.
  */
-void bitonic_merge(int *array, size_t size, size_t start, size_t seq, char flow)
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow)
 {
 	size_t i, jump = seq / 2;
 
@@ -41,20 +43,21 @@ void bitonic_merge(int *array, size_t size, size_t start, size_t seq, char flow)
 		for (i = start; i < start + jump; i++)
 		{
 			if ((flow == UP && array[i] > array[i + jump]) ||
-					(flow == DOWN && array[i] < array[i + jump]))
+			    (flow == DOWN && array[i] < array[i + jump]))
 				swap_ints(array + i, array + i + jump);
 		}
 		bitonic_merge(array, size, start, jump, flow);
 		bitonic_merge(array, size, start + jump, jump, flow);
 	}
 }
+
 /**
- * bitonic_seq - convert an array of integers into a bitonic sequence.
- * @array: An array of integers
- * @size: The size of the array
- * @start: The starting index of a block of the buildong bitonic sequence
- * @seq: The size of a block of the buiding bitonci sequence
- * @flow: The direction to sort the bitonic sequence block in
+ * bitonic_seq - Convert an array of integers into a bitonic sequence.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ * @start: The starting index of a block of the building bitonic sequence.
+ * @seq: The size of a block of the building bitonic sequence.
+ * @flow: The direction to sort the bitonic sequence block in.
  */
 void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
 {
@@ -67,21 +70,22 @@ void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
 		print_array(array + start, seq);
 
 		bitonic_seq(array, size, start, cut, UP);
-		bitonic_seq(array, size, start + cut. cut, DOWN);
+		bitonic_seq(array, size, start + cut, cut, DOWN);
 		bitonic_merge(array, size, start, seq, flow);
 
-		printf("Result [&lu/%lu (%s)]:\n", seq, size, str);
+		printf("Result [%lu/%lu] (%s):\n", seq, size, str);
 		print_array(array + start, seq);
 	}
 }
 
 /**
- * bitonic_sort - sort an array of integers in ascending order
- * 		using the bitonic sort algorithm
- * @array: An array of integers
- * @size: The size of the array
- * Description: prints the array after each swap. Only works for
- * size = 2^k where k >= 0 (that is size aqual to power 2)
+ * bitonic_sort - Sort an array of integers in ascending
+ *                order using the bitonic sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap. Only works for
+ * size = 2^k where k >= 0 (ie. size equal to powers of 2).
  */
 void bitonic_sort(int *array, size_t size)
 {
